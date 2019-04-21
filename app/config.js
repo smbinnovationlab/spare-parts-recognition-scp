96ed76b345ee03780e01762b9dc38ccf3d2ccd9d
CyranoChen <cyrano@arsenalcn.com>
2019-04-21 15:21:35 +0800

    Add dotenv and leave environment variables empty

--- app/config.js
+++ app/config.js
@@ -1,20 +1,22 @@
+require('dotenv').config();
+
 module.exports = {
     getConfigs: function () {
         return {
             "GENERAL": {
-                "THRESHOLD_SIMILAR": Number(process.env.GENERAL_THRESHOLD_SIMILAR || 0.65),
-                "THRESHOLD_NUM_SIMILAR": Number(process.env.GENERAL_THRESHOLD_NUM_SIMILAR || 3)
+                "THRESHOLD_SIMILAR": Number(process.env.GENERAL_THRESHOLD_SIMILAR),
+                "THRESHOLD_NUM_SIMILAR": Number(process.env.GENERAL_THRESHOLD_NUM_SIMILAR)
             },
             "LEONARDO": {
-                "APIKEY": process.env.LEON_APIKEY || "jGMB8R9KPK2MhNv7Tc9vTVGQ1mu7KLB0",
-                "IMAGEFEATUREEXTRACTION_APIURL": process.env.LEON_IMAGEFEATUREEXTRACTION_APIURL || "https://sandbox.api.sap.com/ml/imagefeatureextraction/feature-extraction",
-                "SIMILARITYSCORING_APIURL": process.env.LEON_SIMILARITYSCORING_APIURL || "https://sandbox.api.sap.com/ml/similarityscoring/similarity-scoring"
+                "APIKEY": process.env.LEON_APIKEY,
+                "IMAGEFEATUREEXTRACTION_APIURL": process.env.LEON_IMAGEFEATUREEXTRACTION_APIURL,
+                "SIMILARITYSCORING_APIURL": process.env.LEON_SIMILARITYSCORING_APIURL
             },
             "BUSINESSONE": {
-                "SERVICELAYER_APIURL": process.env.B1_SERVICELAYER_APIURL || "https://52.59.87.94:50000/b1s/v1",
-                "USERNAME": process.env.B1_USERNAME || "manager",
-                "PASSWORD": process.env.B1_PASSWORD || "manager",
-                "COMPANYDB": process.env.SBODEMOGB || "SBODEMOGB"
+                "SERVICELAYER_APIURL": process.env.B1_SERVICELAYER_APIURL,
+                "USERNAME": process.env.B1_USERNAME,
+                "PASSWORD": process.env.B1_PASSWORD,
+                "COMPANYDB": process.env.B1_COMPANYDB
             }
         };
     }
